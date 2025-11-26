d48cd87a75bb815b6af4e1c2046771f8e3ad7bee
muhammaddanielafifi <254107020206@student.polinema.ac.id>
2025-11-26 11:45:32 +0700

    percobaan2

--- pertemuan14/Kafe16.java
+++ pertemuan14/Kafe16.java
@@ -2,7 +2,18 @@ package pertemuan14;
 
 public class Kafe16 {
 
-    static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+        System.out.println("Selamat Datang, " + namaPelanggan + ":");
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian.");
+        } if(kodePromo.equalsIgnoreCase("DISKON50")) {
+            System.out.println("Diskon 50% Berlaku!");
+        } else if(kodePromo.equalsIgnoreCase("DISKON30")) {
+            System.out.println("Diskon 30% Berlaku!");
+        } else {
+            System.out.println("Kode promo invalid");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15.000");
         System.out.println("2. Cappuccino - Rp 20.000");
@@ -14,6 +25,6 @@ public class Kafe16 {
     }
 
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true, "DISKON30");
     }
 }
